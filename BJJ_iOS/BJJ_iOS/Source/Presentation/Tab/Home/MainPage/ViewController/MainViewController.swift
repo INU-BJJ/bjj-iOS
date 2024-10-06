f9e6208790eac0dbb33142a798b5b5ebb49a0fd2
DIN-STUDIO <kht1790@gmail.com>
2024-10-06 17:54:58 +0900

    [Feat] 프로젝트 초기 설정

---
+++ BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/Home/MainPage/ViewController/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BJJ_iOS
+//
+//  Created by HyoTaek on 10/6/24.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .darkGray
+    }
+
+
+}
+
