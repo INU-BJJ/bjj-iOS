98ef496c41146846a1a487fb1bbf2332a5790be0
DIN-STUDIO <kht1790@gmail.com>
2024-10-07 03:50:20 +0900

    [Feat] MainViewController 뼈대 코드 작성

--- BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/Home/MainPage/ViewController/MainViewController.swift
+++ BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/Home/MainPage/ViewController/MainViewController.swift
@@ -6,12 +6,52 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
-class MainViewController: UIViewController {
+final class MainViewController: UIViewController {
 
+    // MARK: Properties
+    
+    // MARK: UI Components
+    
+    // MARK: LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setUI()
+        setAddView()
+        setConstraints()
+    }
+    
+    // MARK: Bind
+    
+    func bind() {
+        
+        // MARK: Action
+        
+        // MARK: State
+        
+    }
+    
+    // MARK: Set UI
+    
+    private func setUI() {
+        view.backgroundColor = .white
+    }
+    
+    // MARK: Set AddViews
+    
+    private func setAddView() {
+        [
+         
+        ].forEach(view.addSubview)
+    }
+    
+    // MARK: Set Constraints
+    
+    private func setConstraints() {
+       
     }
 }
 
