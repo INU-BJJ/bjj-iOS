618a359c8aa31a44f42668065317ba5546136841
DIN-STUDIO <kht1790@gmail.com>
2024-10-07 04:26:39 +0900

    [Feat] 메뉴정보cell 생성

--- BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/Home/View/HomeMenuCell.swift
+++ BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/Home/View/HomeMenuCell.swift
@@ -5,4 +5,51 @@
 //  Created by HyoTaek on 10/7/24.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import Then
+
+final class HomeMenuCell: UICollectionViewCell {
+    
+    // MARK: - UI Components
+    
+    // MARK: - LifeCycle
+        
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setAddView()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Set UI
+        
+    private func setUI() {
+        
+    }
+    
+    // MARK: - Set AddView
+    
+    private func setAddView() {
+        [
+         
+        ].forEach(contentView.addSubview)
+    }
+    
+    // MARK: - Set Constraints
+    
+    private func setConstraints() {
+        
+    }
+    
+    // MARK: - Configuration
+    
+    func configureCell() {
+        
+    }
+}
+
