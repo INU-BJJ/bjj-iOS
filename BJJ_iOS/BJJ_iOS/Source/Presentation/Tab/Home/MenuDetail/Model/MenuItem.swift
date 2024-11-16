b82000f4a2c7ac434ac2ba6e656b9c145cf1c873
HyoTaek <kht1790@gmail.com>
2024-11-16 16:49:20 +0900

    [Refactor] 메뉴 리뷰 model 구조 변경

--- BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/Home/MenuDetail/Model/MenuItem.swift
+++ BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/Home/MenuDetail/Model/MenuItem.swift
@@ -7,6 +7,10 @@
 
 import Foundation
 
+struct ReviewListItem {
+    let reviewList: [Review]
+}
+
 struct Review {
     let menu: MenuItem
     let menuReviewList: [MenuReviewInfo]
@@ -34,40 +38,81 @@ struct UserInfo {
     let nickname: String
 }
 
-extension Review {
-    static let reviewData = Review(
-        menu: MenuItem(
-            menuName: "양상추샐러드/복숭아아이스티1",
-            menuImage: "MenuImage1",
-            menuPrice: "5,500원",
-            menuLiked: true,
-            menuComposition: ["돼지불고기카레", "우동 국물", "찹쌀탕수육", "짜장떡볶이", "깍둑단무지무침", "배추김치", "기장밥"]),
-            menuReviewList:
-            [
-                MenuReviewInfo(
-                    userInfo:
-                        UserInfo(
-                            profileImage: "profile1",
-                            nickname: "떡볶이킬러나는최고야룰루"
-                        ),
-                    menuRating: 5.0,
-                    reviewDate: "2024.08.20",
-                    menuLikedCount: 0,
-                    reviewContent: "핫도그는 냉동인데\n떡볶이는 맛있음\n맛도 있고 가격도 착해서 떡볶이 땡길 때 추천",
-                    reviewImage: "MenuImage1"
-                ),
-                MenuReviewInfo(
-                    userInfo:
-                        UserInfo(
-                            profileImage: "profile2",
-                            nickname: "파괴전차 권효택"
-                        ),
-                    menuRating: 0.1,
-                    reviewDate: "2025.12.31",
-                    menuLikedCount: 99,
-                    reviewContent: "나를 사로잡은 그 맛",
-                    reviewImage: "MenuImage1"
-                )
-            ]
+extension ReviewListItem {
+    static let reviewListData =
+        ReviewListItem(
+            reviewList:
+                [
+                    Review(
+                        menu: MenuItem(
+                                menuName: "양상추샐러드/복숭아아이스티1",
+                                menuImage: "MenuImage1",
+                                menuPrice: "5,500원",
+                                menuLiked: true,
+                                menuComposition: ["돼지불고기카레", "우동 국물", "찹쌀탕수육", "짜장떡볶이", "깍둑단무지무침", "배추김치", "기장밥"]),
+                        menuReviewList:
+                            [
+                                MenuReviewInfo(
+                                    userInfo:
+                                        UserInfo(
+                                            profileImage: "profile1",
+                                            nickname: "떡볶이킬러나는최고야룰루"
+                                        ),
+                                    menuRating: 5.0,
+                                    reviewDate: "2024.08.20",
+                                    menuLikedCount: 0,
+                                    reviewContent: "핫도그는 냉동인데\n떡볶이는 맛있음\n맛도 있고 가격도 착해서 떡볶이 땡길 때 추천",
+                                    reviewImage: "MenuImage1"
+                                ),
+                                MenuReviewInfo(
+                                    userInfo:
+                                        UserInfo(
+                                            profileImage: "profile2",
+                                            nickname: "파괴전차 권효택"
+                                        ),
+                                    menuRating: 0.1,
+                                    reviewDate: "2025.12.31",
+                                    menuLikedCount: 99,
+                                    reviewContent: "나를 사로잡은 그 맛",
+                                    reviewImage: "MenuImage1"
+                                )
+                            ]
+                    ),
+                    Review(
+                        menu: MenuItem(
+                                menuName: "양상추샐러드/복숭아아이스티2",
+                                menuImage: "MenuImage1",
+                                menuPrice: "5,500원",
+                                menuLiked: true,
+                                menuComposition: ["돼지불고기카레", "우동 국물", "찹쌀탕수육", "짜장떡볶이", "깍둑단무지무침", "배추김치", "기장밥"]),
+                        menuReviewList:
+                            [
+                                MenuReviewInfo(
+                                    userInfo:
+                                        UserInfo(
+                                            profileImage: "profile3",
+                                            nickname: "떡볶이킬러나는최고야룰루"
+                                        ),
+                                    menuRating: 5.0,
+                                    reviewDate: "2024.08.20",
+                                    menuLikedCount: 0,
+                                    reviewContent: "핫도그는 냉동인데\n떡볶이는 맛있음\n맛도 있고 가격도 착해서 떡볶이 땡길 때 추천",
+                                    reviewImage: "MenuImage1"
+                                ),
+                                MenuReviewInfo(
+                                    userInfo:
+                                        UserInfo(
+                                            profileImage: "profile4",
+                                            nickname: "파괴전차 권효택"
+                                        ),
+                                    menuRating: 0.1,
+                                    reviewDate: "2025.12.31",
+                                    menuLikedCount: 99,
+                                    reviewContent: "나를 사로잡은 그 맛",
+                                    reviewImage: "MenuImage1"
+                                )
+                            ]
+                    )
+                ]
         )
 }
