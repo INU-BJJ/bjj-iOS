63dfc2a6bf6d665034f2e652f2388e1577f77e81
HyoTaek <kht1790@gmail.com>
2024-12-26 16:30:28 +0900

    [UI] 식당 목록 순서 변경

--- BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/Home/Home/Model/HomeSection.swift
+++ BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/Home/Home/Model/HomeSection.swift
@@ -21,9 +21,11 @@ struct HomeMenuModel: Hashable {
 
 extension HomeCafeteriaModel {
     static let cafeteria = [
-        HomeCafeteriaModel(text: "인천대 학생식당"), 
-        HomeCafeteriaModel(text: "인천대 교직원식당"),
-        HomeCafeteriaModel(text: "제1기숙사식당")
+        HomeCafeteriaModel(text: "인천대 학생식당"),
+        HomeCafeteriaModel(text: "2호관 식당"),
+        HomeCafeteriaModel(text: "제1기숙사식당"),
+        HomeCafeteriaModel(text: "27호관식당"),
+        HomeCafeteriaModel(text: "사범대식당")
     ]
 }
 
