cad29e392e60419a8861cd34d6ed2e612ccfb216
HyoTaek <kht1790@gmail.com>
2025-02-02 17:42:28 +0900

    [Feat] MyReviewVC 생성

---
+++ BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/Review/MyReview/ViewController/MyReviewViewController.swift
@@ -0,0 +1,45 @@
+//
+//  MyReviewViewController.swift
+//  BJJ_iOS
+//
+//  Created by HyoTaek on 2/2/25.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class MyReviewViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - UI Components
+    
+    // MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUI()
+        setAddView()
+        setConstraints()
+    }
+    
+    // MARK: - Set UI
+    
+    private func setUI() {
+        
+    }
+    
+    // MARK: - Set AddViews
+    
+    private func setAddView() {
+        
+    }
+    
+    // MARK: - Set Constraints
+    
+    private func setConstraints() {
+        
+    }
+}
