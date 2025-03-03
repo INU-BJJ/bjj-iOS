770f668d93557c60c7306a4239ee00ea0d5d9e6d
HyoTaek <kht1790@gmail.com>
2025-03-03 18:06:32 +0900

    [Feat] 리뷰 작성시 선택한 식당에 대한 메뉴 fetch API 함수 구현

--- BJJ_iOS/BJJ_iOS/Source/Network/API/Review/ReviewWrite/ReviewWriteAPI.swift
+++ BJJ_iOS/BJJ_iOS/Source/Network/API/Review/ReviewWrite/ReviewWriteAPI.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+final class ReviewWriteAPI {
+    static func fetchReviewWriteMenuInfo(cafeteriaName: String, completion: @escaping (Result<[ReviewWriteMenuInfo], Error>) -> Void) {
+            networkRequest(
+                urlStr: ReviewWriteAddress.fetchMenuList.url,
+                method: .get,
+                data: nil,
+                model: [ReviewWriteMenuInfo].self,
+                query: ["cafeteriaName": cafeteriaName],
+                completion: completion
+            )
+        }
+}
