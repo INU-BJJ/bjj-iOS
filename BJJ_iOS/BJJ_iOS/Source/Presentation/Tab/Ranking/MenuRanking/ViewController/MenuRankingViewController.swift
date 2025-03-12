fba388d870891531c95e22b6b1dcc35c9d23393d
HyoTaek <kht1790@gmail.com>
2025-03-12 18:44:05 +0900

    [Feat] 초기 MenuRankingVC 생성

---
+++ BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/Ranking/MenuRanking/ViewController/MenuRankingViewController.swift
@@ -0,0 +1,47 @@
+//
+//  MenuRankingViewController.swift
+//  BJJ_iOS
+//
+//  Created by HyoTaek on 3/12/25.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class MenuRankingViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - UI Components
+    
+    // MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setViewController()
+        setAddView()
+        setConstraints()
+    }
+    
+    // MARK: - Set UI
+    
+    private func setViewController() {
+        
+    }
+    
+    // MARK: - Set AddViews
+    
+    private func setAddView() {
+        [
+            
+        ].forEach(view.addSubview)
+    }
+    
+    // MARK: - Set Constraints
+    
+    private func setConstraints() {
+        
+    }
+}
