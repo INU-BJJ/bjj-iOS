e784be34bbc0b0f496fd0fb99e7c13ba0830f168
HyoTaek <kht1790@gmail.com>
2025-03-17 20:24:27 +0900

    [UI] reviewModal창의 리뷰 텍스트, 이미지 추가

--- BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/Ranking/ReviewModal/View/ReviewModalImageCell.swift
+++ BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/Ranking/ReviewModal/View/ReviewModalImageCell.swift
@@ -5,4 +5,71 @@
 //  Created by HyoTaek on 3/17/25.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import Then
+import Kingfisher
+
+final class ReviewModalImageCell: UICollectionViewCell, ReuseIdentifying {
+    
+    // MARK: - Properties
+    
+    private let cornerRadius: CGFloat = 11
+    
+    // MARK: - UI Components
+    
+    private let reviewImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
+        $0.clipsToBounds = true
+    }
+    
+    // MARK: - Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setAddView()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Set AddView
+    
+    private func setAddView() {
+        [
+            reviewImageView
+        ].forEach(contentView.addSubview)
+    }
+    
+    // MARK: - Set Constraints {
+    
+    private func setConstraints() {
+        reviewImageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    // MARK: - Configure Cell
+    
+    func configureReviewImageCell(with imageName: String, cornerStyle: UIRectCorner) {
+        // TODO: 서버 이미지로 교체
+//        reviewImageView.kf.setImage(with: URL(string: "\(baseURL.imageURL)\(imageName)"))
+        reviewImageView.image = UIImage(named: imageName)
+        applyCorners(cornerStyle: cornerStyle)
+    }
+    
+    private func applyCorners(cornerStyle: UIRectCorner) {
+        let path = UIBezierPath(
+            roundedRect: bounds,
+            byRoundingCorners: cornerStyle,
+            cornerRadii: CGSize(width: cornerRadius, height: cornerRadius)
+        )
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        reviewImageView.layer.mask = mask
+    }
+}
+
--- BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/Ranking/ReviewModal/ViewController/ReviewModalViewController.swift
+++ BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/Ranking/ReviewModal/ViewController/ReviewModalViewController.swift
@@ -13,6 +13,8 @@ final class ReviewModalViewController: UIViewController {
     
     // MARK: - Properties
     
+    private let reviewImages: [String] = ["MenuImage2"]
+    
     // MARK: - UI Components
     
     private let reviewModalStackView = UIStackView().then {
@@ -23,15 +25,27 @@ final class ReviewModalViewController: UIViewController {
         $0.backgroundColor = .white
         $0.layoutMargins = UIEdgeInsets(top: 17, left: 14, bottom: 17, right: 14)
         $0.isLayoutMarginsRelativeArrangement = true
+        $0.layer.cornerRadius = 17
     }
     
     private let reviewInfoView = ReviewInfoView()
     
-    private let reviewTextView = UITextView().then {
-        $0.setTextViewUI("떡볶이 맛있당", font: .pretendard_medium, size: 13, color: .black)
-        $0.isScrollEnabled = false
-        $0.textContainerInset = UIEdgeInsets.zero
-        $0.textContainer.lineFragmentPadding = 0
+    private let reviewTextLabel = UILabel().then {
+        $0.setLabelUI("핫도그는 냉동인데\n떡볶이는 맛있음\n맛도 있고 가격도 착해서 떡볶이 땡길 때 추천", font: .pretendard_medium, size: 13, color: .black)
+        $0.setLineSpacing(kernValue: 0.13, lineHeightMultiple: 1.1)
+        $0.numberOfLines = 0
+        $0.lineBreakMode = .byWordWrapping
+    }
+    
+    private lazy var reviewImageCollectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: createImageLayout()
+    ).then {
+        $0.register(ReviewModalImageCell.self, forCellWithReuseIdentifier: ReviewModalImageCell.reuseIdentifier)
+        $0.dataSource = self
+        $0.delegate = self
+        $0.showsHorizontalScrollIndicator = false
+        $0.alwaysBounceVertical = false
     }
     
     // MARK: - LifeCycle
@@ -59,7 +73,8 @@ final class ReviewModalViewController: UIViewController {
         
         [
             reviewInfoView,
-            reviewTextView
+            reviewTextLabel,
+            reviewImageCollectionView
         ].forEach(reviewModalStackView.addArrangedSubview)
     }
     
@@ -67,7 +82,7 @@ final class ReviewModalViewController: UIViewController {
     
     private func setConstraints() {
         reviewModalStackView.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide).offset(132)
+            $0.top.equalToSuperview().offset(226)
             $0.horizontalEdges.equalToSuperview().inset(16)
             $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(108)
         }
@@ -75,5 +90,90 @@ final class ReviewModalViewController: UIViewController {
         reviewInfoView.snp.makeConstraints {
             $0.height.equalTo(41)
         }
+        
+        reviewImageCollectionView.snp.makeConstraints {
+            $0.height.equalTo(283)
+        }
+    }
+    
+    // MARK: - Create Layout
+    
+    private func createImageLayout() -> UICollectionViewCompositionalLayout {
+        let layout = UICollectionViewCompositionalLayout {
+            (sectionIndex, layoutEnvironment) -> NSCollectionLayoutSection? in
+            
+            // TODO: absolute 없애기
+            // TODO: 너비가 이전에 쓰던 것과 달라져서 업데이트 하기
+            let absoluteItemWidth: CGFloat
+            let absoluteGroupWidth: CGFloat
+            
+            switch self.reviewImages.count {
+            case 1:
+                absoluteItemWidth = 301
+                absoluteGroupWidth = 301
+            case 2:
+                absoluteItemWidth = 149.5
+                absoluteGroupWidth = 301
+            case 3:
+//                fractionalWidth = 0.4738047138
+                absoluteItemWidth = 140.72
+                absoluteGroupWidth = 426.16
+            case 4:
+//                fractionalWidth = 0.4738047138
+                absoluteItemWidth = 140.72
+                absoluteGroupWidth = 568.88
+            case 5:
+//                fractionalWidth = 0.4738047138
+                absoluteItemWidth = 140.72
+                absoluteGroupWidth = 711.59
+            default:
+//                absoluteItemWidth = 1.0
+                absoluteItemWidth = 140.72
+                absoluteGroupWidth = 301
+            }
+            
+            let itemSize = NSCollectionLayoutSize(widthDimension: .absolute(absoluteItemWidth), heightDimension: .fractionalHeight(1.0))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            
+            let groupSize = NSCollectionLayoutSize(widthDimension: .absolute(absoluteGroupWidth), heightDimension: .fractionalHeight(1.0))
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+            group.interItemSpacing = .fixed(2)
+            
+            let section = NSCollectionLayoutSection(group: group)
+            section.orthogonalScrollingBehavior = .continuous
+            
+            return section
+        }
+        
+        return layout
+    }
+}
+
+extension ReviewModalViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return reviewImages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ReviewModalImageCell.reuseIdentifier, for: indexPath) as! ReviewModalImageCell
+        var cornerStyle: UIRectCorner = []
+        
+        if reviewImages.count > 1 {
+            if indexPath.row == 0 {
+                cornerStyle = [.topLeft, .bottomLeft]
+            } else if indexPath.row == reviewImages.count - 1 {
+                cornerStyle = [.topRight, .bottomRight]
+            }
+        } else {
+            cornerStyle = [.allCorners]
+        }
+
+        cell.configureReviewImageCell(with: reviewImages[indexPath.row], cornerStyle: cornerStyle)
+        
+        return cell
     }
 }
