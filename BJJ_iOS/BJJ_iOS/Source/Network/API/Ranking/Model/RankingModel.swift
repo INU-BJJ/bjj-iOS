58e50f4a1faf92404566bfb895f08344257dd400
HyoTaek <kht1790@gmail.com>
2025-03-18 16:31:15 +0900

    [Feat] 랭킹탭 네트워킹 모델, 주소 구현

---
+++ BJJ_iOS/BJJ_iOS/Source/Network/API/Ranking/Model/RankingModel.swift
@@ -0,0 +1,40 @@
+//
+//  RankingModel.swift
+//  BJJ_iOS
+//
+//  Created by HyoTaek on 3/17/25.
+//
+
+import Foundation
+
+struct MenuRankingInfo: Hashable, Codable {
+    let menuID: Int
+    let menuName: String
+    let menuRating: Double
+    let cafeteriaName: String
+    let cafeteriaCorner: String
+    let bestReviewID: Int
+    let reviewImage: String
+    let updatedDate: String
+    
+    enum CodingKeys: String, CodingKey {
+        case menuID = "menuId"
+        case menuName
+        case menuRating
+        case cafeteriaName
+        case cafeteriaCorner
+        case bestReviewID = "bestReviewId"
+        case reviewImage = "reviewImageName"
+        case updatedDate = "updatedAt"
+    }
+}
+
+struct MenuRankingList: Hashable, Codable {
+    let menuRankingList: [MenuRankingInfo]
+    let isLastPage: Bool
+    
+    enum CodingKeys: String, CodingKey {
+        case menuRankingList = "menuRankingDetailList"
+        case isLastPage = "lastPage"
+    }
+}
