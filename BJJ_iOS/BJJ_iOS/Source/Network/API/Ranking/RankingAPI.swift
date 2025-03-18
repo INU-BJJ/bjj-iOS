be083792a31f0f3c8b0a478b40ac11f7e07a8ffb
HyoTaek <kht1790@gmail.com>
2025-03-18 16:34:58 +0900

    [Feat] 메뉴 랭킹 fetch함수 정의

--- BJJ_iOS/BJJ_iOS/Source/Network/API/Ranking/RankingAPI.swift
+++ BJJ_iOS/BJJ_iOS/Source/Network/API/Ranking/RankingAPI.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+final class RankingAPI {
+    static func fetchRankingList(pageNumber: Int, pageSize: Int, completion: @escaping (Result<MenuRankingList, Error>) -> Void) {
+            networkRequest(
+                urlStr: RankingAddress.fetchMenuRanking.url,
+                method: .get,
+                data: nil,
+                model: MenuRankingList.self,
+                query: [
+                    "pageNumber": pageNumber,
+                    "pageSize": pageSize
+                ],
+                completion: completion
+            )
+        }
+}
