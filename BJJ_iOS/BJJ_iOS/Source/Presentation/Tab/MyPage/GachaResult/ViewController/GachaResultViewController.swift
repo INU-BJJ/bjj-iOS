389142280f31c1757392bb3413fb46a70d218c8c
HyoTaek <kht1790@gmail.com>
2025-04-14 13:26:49 +0900

    [Feat] 아이템 뽑기 후 캐릭터를 UI에 적용

--- BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/MyPage/GachaResult/ViewController/GachaResultViewController.swift
+++ BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/MyPage/GachaResult/ViewController/GachaResultViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import SnapKit
 import Then
+import SDWebImage
 
 final class GachaResultViewController: UIViewController {
     
@@ -15,11 +16,15 @@ final class GachaResultViewController: UIViewController {
     
     // MARK: - UI Components
     
+    private let testDrawnCharacter = UIImageView()
+    
     // MARK: - LifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // TODO: 캐릭터인지 배경인지 구분해서 POST 요청 보내기
+        postItemGacha(itemType: "CHARACTER", completion: setUI)
         setViewController()
         setAddView()
         setConstraints()
@@ -35,13 +40,44 @@ final class GachaResultViewController: UIViewController {
     
     private func setAddView() {
         [
-            
+            testDrawnCharacter
         ].forEach(view.addSubview)
     }
     
     // MARK: - Set Constraints
     
     private func setConstraints() {
-        
+        testDrawnCharacter.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+    
+    // MARK: - Set UI
+    
+    private func setUI(_ itemImage: String) {
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else { return }
+            guard let characterURL = URL(string: baseURL.characterImageURL + itemImage) else { return }
+            
+            self.testDrawnCharacter.sd_setImage(
+                with: characterURL,
+                placeholderImage: nil,
+                options: [.retryFailed, .continueInBackground]
+            )
+        }
+    }
+    
+    // MARK: - Post API
+    
+    private func postItemGacha(itemType: String, completion: @escaping (_ itemImage: String) -> Void) {
+        GachaResultAPI.postItemGacha(itemType: itemType) { result in
+            switch result {
+            case .success(let itemInfo):
+                completion(itemInfo.itemImage)
+                
+            case .failure(let error):
+                print("[GachaResultVC] Error: \(error.localizedDescription)")
+            }
+        }
     }
 }
