56af091e355e76c8ab92072ceba435d3ccd61fa2
HyoTaek <kht1790@gmail.com>
2025-05-19 16:05:19 +0900

    [Feat] 메뉴 랭킹 업데이트 날짜 표시 기능 구현

--- BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/Ranking/MenuRanking/ViewController/MenuRankingViewController.swift
+++ BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/Ranking/MenuRanking/ViewController/MenuRankingViewController.swift
@@ -36,9 +36,7 @@ final class MenuRankingViewController: UIViewController {
     }
     
     private let dateLabel = UILabel().then {
-        // TODO: 서버 정보 fetch
-        // TODO: 서버에서 menu마다 업데이트 날짜를 주고 있는데, 어짜피 메뉴 랭킹은 매일마다 업데이트 되니까 따로 보내달라고 요청
-        $0.setLabelUI("2024.12.26", font: .pretendard_medium, size: 11, color: .darkGray)
+        $0.setLabelUI("", font: .pretendard_medium, size: 11, color: .darkGray)
     }
     
     private let updateLabel = UILabel().then {
@@ -74,6 +72,7 @@ final class MenuRankingViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        updateDate()
         fetchMenuRanking(pageNumber: currentPageNumber, pageSize: pageSize)
     }
     
@@ -131,6 +130,20 @@ final class MenuRankingViewController: UIViewController {
     private func setStackView() {
         dateUpdateStackView.setCustomSpacing(5, after: dateLabel)
         dateUpdateStackView.setCustomSpacing(4, after: updateLabel)
+    }
+    
+    // MARK: - Update Date
+    
+    private func updateDate() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy.MM.dd"
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+        dateFormatter.timeZone = TimeZone(identifier: "Asia/Seoul")
+        
+        let today = Date()
+        let todayDate = dateFormatter.string(from: today)
+        
+        dateLabel.text = todayDate
     }
     
     // MARK: - API Function
