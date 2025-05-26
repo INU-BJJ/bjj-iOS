81661df2131fefb0cc688b21413e7eedb5859db9
HyoTaek <kht1790@gmail.com>
2025-05-26 15:21:27 +0900

    [Refactor] 메뉴 리뷰 이미지 collectionView를 flowlayout으로 리팩토링

--- BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/Home/MenuDetail/View/MenuReviewListCell/MenuReviewListCell.swift
+++ BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/Home/MenuDetail/View/MenuReviewListCell/MenuReviewListCell.swift
@@ -13,6 +13,8 @@ final class MenuReviewListCell: UICollectionViewCell, ReuseIdentifying {
     
     // MARK: - Properties
     
+    private var reviewImages: [String] = []
+    
     // MARK: - UI Components
     
     private let reviewStackView = UIStackView().then {
@@ -26,6 +28,16 @@ final class MenuReviewListCell: UICollectionViewCell, ReuseIdentifying {
     private let reviewCommentLabel = UILabel().then {
         $0.setLabelUI("", font: .pretendard_medium, size: 13, color: .black)
         $0.numberOfLines = 0
+    }
+    
+    private lazy var reviewImageCollectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: UICollectionViewLayout()
+    ).then {
+        $0.register(MenuReviewImageCell.self, forCellWithReuseIdentifier: MenuReviewImageCell.reuseIdentifier)
+        $0.dataSource = self
+        $0.showsHorizontalScrollIndicator = false
+        $0.alwaysBounceVertical = false
     }
     
     // MARK: - Life Cycle
@@ -58,7 +70,7 @@ final class MenuReviewListCell: UICollectionViewCell, ReuseIdentifying {
         [
             menuReviewInfoView,
             reviewCommentLabel,
-//            reviewImageCollectionView,
+            reviewImageCollectionView,
 //            reviewHashTagCollectionView
         ].forEach(reviewStackView.addArrangedSubview)
     }
@@ -68,19 +80,99 @@ final class MenuReviewListCell: UICollectionViewCell, ReuseIdentifying {
     private func setConstraints() {
         reviewStackView.snp.makeConstraints {
             $0.edges.equalToSuperview()
-            $0.width.equalTo(400)
+            $0.width.equalTo(390)
         }
         
         menuReviewInfoView.snp.makeConstraints {
             $0.width.equalToSuperview()
             $0.height.equalTo(41)
         }
+        
+        reviewImageCollectionView.snp.makeConstraints {
+            $0.width.equalToSuperview()
+            $0.height.equalTo(250)
+        }
     }
     
     // MARK: - Configure Cell
     
     func configure(with menuReview: MenuDetailModel) {
+        self.reviewImages = menuReview.reviewImage ?? []
+        
+        let reviewImageCount = menuReview.reviewImage?.count ?? 0
+        let flowLayout = createFlowLayout(reviewImageCount: reviewImageCount)
+        
         menuReviewInfoView.setUI(with: menuReview)
         reviewCommentLabel.text = menuReview.reviewComment
+        reviewImageCollectionView.setCollectionViewLayout(flowLayout, animated: false)
+        
+        if reviewImages.isEmpty {
+            reviewImageCollectionView.removeFromSuperview()
+        }
+        
+        DispatchQueue.main.async {
+            self.reviewImageCollectionView.reloadData()
+        }
+    }
+    
+    // MARK: - Create FlowLayout
+    
+    func createFlowLayout(reviewImageCount: Int) -> UICollectionViewFlowLayout {
+        let layout = UICollectionViewFlowLayout()
+        
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 2
+        
+        switch reviewImageCount {
+        case 1:
+            layout.itemSize = CGSize(width: 301, height: 250)
+        case 2:
+            layout.itemSize = CGSize(width: 149.5, height: 250)
+        case 3:
+            layout.itemSize = CGSize(width: 140.72, height: 250)
+        case 4:
+            layout.itemSize = CGSize(width: 140.72, height: 250)
+        default:
+            layout.itemSize = CGSize(width: 301, height: 250)
+        }
+
+        // section에 대한 여백을 설정합니다. (콘텐츠뷰 여백)
+//        layout.sectionInset = UIEdgeInsets(top: 30, left: 30, bottom: 30, right: 30)
+        
+        
+        // header, footer 크기 지정
+        // 스크롤 방향에 따라서 크기가 영향을 받는다. (vertical은 height값만 갖고 계산, horizontal 일 경우 width만 영향을 받는다)
+//        layout.headerReferenceSize = CGSize(width: 50, height: 100)
+//        layout.footerReferenceSize = CGSize(width: 50, height: 100)
+        // header, footer를 고정 시킴 (기본값 false)
+//        layout.sectionHeadersPinToVisibleBounds = true
+//        layout.sectionFootersPinToVisibleBounds = true
+        
+        return layout
+    }
+}
+
+extension MenuReviewListCell: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return reviewImages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MenuReviewImageCell.reuseIdentifier, for: indexPath) as! MenuReviewImageCell
+        var cornerStyle: UIRectCorner = []
+        
+        if reviewImages.count > 1 {
+            if indexPath.row == 0 {
+                cornerStyle = [.topLeft, .bottomLeft]
+            } else if indexPath.row == reviewImages.count - 1 {
+                cornerStyle = [.topRight, .bottomRight]
+            }
+        } else {
+            cornerStyle = [.allCorners]
+        }
+
+        cell.configureReviewImageCell(with: reviewImages[indexPath.row], cornerStyle: cornerStyle)
+        
+        return cell
     }
 }
