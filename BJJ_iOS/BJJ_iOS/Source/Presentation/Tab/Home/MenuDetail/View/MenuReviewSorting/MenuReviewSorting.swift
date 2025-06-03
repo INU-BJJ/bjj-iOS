4dbdc4dc4a95331a9db6ddbaf181dd8f1f961e7f
HyoTaek <kht1790@gmail.com>
2025-06-03 15:10:25 +0900

    [Comment] 추후 collectionView Layout 리팩토링을 위한 주석 처리
    - shadowContainerView 주석 처리
    - reviewSortingTableView 주석 처리
    - hitTest 주석 처리
    - setShadow 주석 처리
    - toggleTableView 주석 처리
    - UITableView Extension 주석 처리

--- BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/Home/MenuDetail/View/MenuReviewSorting/MenuReviewSorting.swift
+++ BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/Home/MenuDetail/View/MenuReviewSorting/MenuReviewSorting.swift
@@ -57,35 +57,35 @@ final class MenuReviewSorting: UICollectionViewCell, ReuseIdentifying {
         $0.image = UIImage(named: "toggle")
     }
     
-    private lazy var shadowContainerView = UIView().then {
-        let firstLayer = CALayer()
-        firstLayer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.2).cgColor
-        firstLayer.shadowOpacity = 1
-        firstLayer.shadowRadius = 2.5
-        firstLayer.shadowOffset = CGSize(width: 0, height: 1)
-
-        let secondLayer = CALayer()
-        secondLayer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.15).cgColor
-        secondLayer.shadowOpacity = 1
-        secondLayer.shadowRadius = 3.9
-        secondLayer.shadowOffset = CGSize(width: 0, height: 2)
-        
-        $0.layer.insertSublayer(firstLayer, at: 0)
-        $0.layer.insertSublayer(secondLayer, at: 1)
-        $0.layer.setValue(firstLayer, forKey: "firstLayerShadow")
-        $0.layer.setValue(secondLayer, forKey: "secondLayerShadow")
-        $0.backgroundColor = .clear
-        $0.isHidden = true
-    }
-    
-    private lazy var reviewSortingTableView = UITableView().then {
-        $0.register(MenuReviewSortingCell.self, forCellReuseIdentifier: MenuReviewSortingCell.reuseIdentifier)
-        $0.dataSource = self
-        $0.delegate = self
-        $0.separatorStyle = .none
-        $0.layer.cornerRadius = 10
-        $0.clipsToBounds = true
-    }
+//    private lazy var shadowContainerView = UIView().then {
+//        let firstLayer = CALayer()
+//        firstLayer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.2).cgColor
+//        firstLayer.shadowOpacity = 1
+//        firstLayer.shadowRadius = 2.5
+//        firstLayer.shadowOffset = CGSize(width: 0, height: 1)
+//
+//        let secondLayer = CALayer()
+//        secondLayer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.15).cgColor
+//        secondLayer.shadowOpacity = 1
+//        secondLayer.shadowRadius = 3.9
+//        secondLayer.shadowOffset = CGSize(width: 0, height: 2)
+//        
+//        $0.layer.insertSublayer(firstLayer, at: 0)
+//        $0.layer.insertSublayer(secondLayer, at: 1)
+//        $0.layer.setValue(firstLayer, forKey: "firstLayerShadow")
+//        $0.layer.setValue(secondLayer, forKey: "secondLayerShadow")
+//        $0.backgroundColor = .clear
+//        $0.isHidden = true
+//    }
+//    
+//    private lazy var reviewSortingTableView = UITableView().then {
+//        $0.register(MenuReviewSortingCell.self, forCellReuseIdentifier: MenuReviewSortingCell.reuseIdentifier)
+//        $0.dataSource = self
+//        $0.delegate = self
+//        $0.separatorStyle = .none
+//        $0.layer.cornerRadius = 10
+//        $0.clipsToBounds = true
+//    }
     
     // MARK: - Life Cycle
     
@@ -100,22 +100,22 @@ final class MenuReviewSorting: UICollectionViewCell, ReuseIdentifying {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
-        // 현재 뷰에서 터치 이벤트 감지
-        if let view = super.hitTest(point, with: event) {
-            return view //현재 뷰가 터치 가능하면 그대로 반환 (터치 가능 범위 확장 필요 x)
-        }
-
-        // 현재 뷰에서 터치 감지 실패 -> 내부 서브뷰 검사
-        for subview in subviews {
-            let convertedPoint = subview.convert(point, from: self)
-            if let hitView = subview.hitTest(convertedPoint, with: event) {
-                return hitView // 서브뷰가 터치 가능하면 반환
-            }
-        }
-
-        return nil // 터치된 곳이 없으면 nil 반환
-    }
+//    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+//        // 현재 뷰에서 터치 이벤트 감지
+//        if let view = super.hitTest(point, with: event) {
+//            return view //현재 뷰가 터치 가능하면 그대로 반환 (터치 가능 범위 확장 필요 x)
+//        }
+//
+//        // 현재 뷰에서 터치 감지 실패 -> 내부 서브뷰 검사
+//        for subview in subviews {
+//            let convertedPoint = subview.convert(point, from: self)
+//            if let hitView = subview.hitTest(convertedPoint, with: event) {
+//                return hitView // 서브뷰가 터치 가능하면 반환
+//            }
+//        }
+//
+//        return nil // 터치된 곳이 없으면 nil 반환
+//    }
     
     // MARK: - Set AddView
     
@@ -123,7 +123,7 @@ final class MenuReviewSorting: UICollectionViewCell, ReuseIdentifying {
         [
             onlyPhotoReviewStackView,
             reviewToggleButton,
-            shadowContainerView
+//            shadowContainerView
         ].forEach(addSubview)
         
         [
@@ -136,9 +136,9 @@ final class MenuReviewSorting: UICollectionViewCell, ReuseIdentifying {
             toggleImage
         ].forEach(reviewToggleButton.addArrangedSubview)
         
-        [
-            reviewSortingTableView
-        ].forEach(shadowContainerView.addSubview)
+//        [
+//            reviewSortingTableView
+//        ].forEach(shadowContainerView.addSubview)
     }
     
     // MARK: - Set Constraints
@@ -154,35 +154,35 @@ final class MenuReviewSorting: UICollectionViewCell, ReuseIdentifying {
             $0.trailing.equalToSuperview().inset(22)
         }
         
-        shadowContainerView.snp.makeConstraints {
-            $0.top.equalTo(reviewToggleButton.snp.bottom).offset(11)
-            $0.trailing.equalTo(reviewToggleButton.snp.trailing).offset(3)
-            $0.width.equalTo(134)
-            $0.height.equalTo(93)
-        }
-        
-        reviewSortingTableView.snp.makeConstraints {
-            $0.edges.equalToSuperview()
-        }
+//        shadowContainerView.snp.makeConstraints {
+//            $0.top.equalTo(reviewToggleButton.snp.bottom).offset(11)
+//            $0.trailing.equalTo(reviewToggleButton.snp.trailing).offset(3)
+//            $0.width.equalTo(134)
+//            $0.height.equalTo(93)
+//        }
+//        
+//        reviewSortingTableView.snp.makeConstraints {
+//            $0.edges.equalToSuperview()
+//        }
     }
     
     // MARK: - Set Shadow
     
-    private func setShadow() {
-        let shadowPath = UIBezierPath(roundedRect: shadowContainerView.bounds, cornerRadius: 10).cgPath
-
-        if let firstLayer = shadowContainerView.layer.value(forKey: "firstLayerShadow") as? CALayer {
-            firstLayer.shadowPath = shadowPath
-            firstLayer.bounds = shadowContainerView.bounds
-            firstLayer.position = CGPoint(x: shadowContainerView.bounds.midX, y: shadowContainerView.bounds.midY)
-        }
-
-        if let secondLayer = shadowContainerView.layer.value(forKey: "secondLayerShadow") as? CALayer {
-            secondLayer.shadowPath = shadowPath
-            secondLayer.bounds = shadowContainerView.bounds
-            secondLayer.position = CGPoint(x: shadowContainerView.bounds.midX, y: shadowContainerView.bounds.midY)
-        }
-    }
+//    private func setShadow() {
+//        let shadowPath = UIBezierPath(roundedRect: shadowContainerView.bounds, cornerRadius: 10).cgPath
+//
+//        if let firstLayer = shadowContainerView.layer.value(forKey: "firstLayerShadow") as? CALayer {
+//            firstLayer.shadowPath = shadowPath
+//            firstLayer.bounds = shadowContainerView.bounds
+//            firstLayer.position = CGPoint(x: shadowContainerView.bounds.midX, y: shadowContainerView.bounds.midY)
+//        }
+//
+//        if let secondLayer = shadowContainerView.layer.value(forKey: "secondLayerShadow") as? CALayer {
+//            secondLayer.shadowPath = shadowPath
+//            secondLayer.bounds = shadowContainerView.bounds
+//            secondLayer.position = CGPoint(x: shadowContainerView.bounds.midX, y: shadowContainerView.bounds.midY)
+//        }
+//    }
     
     // MARK: - Objc Function
     
@@ -194,67 +194,67 @@ final class MenuReviewSorting: UICollectionViewCell, ReuseIdentifying {
     }
     
     @objc private func didTapReviewSorting() {
-        toggleTableView()
-        
-        if isReviewSortingExpanded {
-            setShadow()
-        }
+        delegate?.didTapReviewSort()
+//        toggleTableView()
+//        
+//        if isReviewSortingExpanded {
+//            setShadow()
+//        }
     }
     
-    // MARK: - TableView Toggle
-    
-    private func toggleTableView() {
-        isReviewSortingExpanded.toggle()
-        
-        UIView.animate(withDuration: 0.5) {
-            self.shadowContainerView.isHidden = !self.isReviewSortingExpanded
-        }
-    }
+//    // MARK: - TableView Toggle
+//    
+//    private func toggleTableView() {
+//        isReviewSortingExpanded.toggle()
+//        
+//        UIView.animate(withDuration: 0.5) {
+//            self.shadowContainerView.isHidden = !self.isReviewSortingExpanded
+//        }
+//    }
 }
 
 // MARK: - UITableView
 
-extension MenuReviewSorting: UITableViewDataSource, UITableViewDelegate {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return sortingCriteria.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: MenuReviewSortingCell.reuseIdentifier, for: indexPath) as! MenuReviewSortingCell
-        let isLastCell = indexPath.row == tableView.numberOfRows(inSection: indexPath.section) - 1
-        
-        cell.selectionStyle = .none
-        cell.configureMenuReviewSortingCell(
-            sortingCriteria: sortingCriteria[indexPath.row],
-            isSelected: indexPath.row == selectedSortingIndex
-        )
-        cell.hideSeparator(isLastCell)
-        
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 31
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        toggleLabel.text = sortingCriteria[indexPath.row]
-        toggleTableView()
-        selectedSortingIndex = indexPath.row
-        
-        let sortingCriteria: String
-        
-        switch selectedSortingIndex {
-        case 0:
-            sortingCriteria = "BEST_MATCH"
-        case 1:
-            sortingCriteria = "MOST_LIKED"
-        default:
-            sortingCriteria = "NEWEST_FIRST"
-        }
-        
-        delegate?.didReviewSort(sortingCriteria: sortingCriteria)
-        tableView.reloadData()
-    }
-}
-
+//extension MenuReviewSorting: UITableViewDataSource, UITableViewDelegate {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return sortingCriteria.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: MenuReviewSortingCell.reuseIdentifier, for: indexPath) as! MenuReviewSortingCell
+//        let isLastCell = indexPath.row == tableView.numberOfRows(inSection: indexPath.section) - 1
+//        
+//        cell.selectionStyle = .none
+//        cell.configureMenuReviewSortingCell(
+//            sortingCriteria: sortingCriteria[indexPath.row],
+//            isSelected: indexPath.row == selectedSortingIndex
+//        )
+//        cell.hideSeparator(isLastCell)
+//        
+//        return cell
+//    }
+//    
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 31
+//    }
+//    
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        toggleLabel.text = sortingCriteria[indexPath.row]
+//        toggleTableView()
+//        selectedSortingIndex = indexPath.row
+//        
+//        let sortingCriteria: String
+//        
+//        switch selectedSortingIndex {
+//        case 0:
+//            sortingCriteria = "BEST_MATCH"
+//        case 1:
+//            sortingCriteria = "MOST_LIKED"
+//        default:
+//            sortingCriteria = "NEWEST_FIRST"
+//        }
+//        
+//        delegate?.didReviewSort(sortingCriteria: sortingCriteria)
+//        tableView.reloadData()
+//    }
+//}
