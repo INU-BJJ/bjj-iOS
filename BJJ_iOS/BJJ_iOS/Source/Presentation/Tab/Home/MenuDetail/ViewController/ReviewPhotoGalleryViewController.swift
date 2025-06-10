b8f241a664da69a4606db0d0e7f55fcf95dd9d33
HyoTaek <kht1790@gmail.com>
2025-06-10 16:34:01 +0900

    [Feat] 리뷰 모아보기 vc 생성

---
+++ BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/Home/MenuDetail/ViewController/ReviewPhotoGalleryViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ReviewPhotoGalleryViewController.swift
+//  BJJ_iOS
+//
+//  Created by HyoTaek on 6/9/25.
+//
+
+import UIKit
+import SnapKit
+import Then
+import Kingfisher
+
+final class ReviewPhotoGalleryViewController: UIViewController {
+
+    // MARK: - Properties
+    
+    // MARK: - UI Components
+    
+    // MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setViewController()
+        setAddView()
+        setConstraints()
+    }
+    
+    // MARK: - Set ViewController
+    
+    private func setViewController() {
+        view.backgroundColor = .white
+        setBackNaviBar("리뷰 사진 모아보기")
+    }
+    
+    // MARK: - Set AddViews
+    
+    private func setAddView() {
+        [
+            
+        ].forEach(view.addSubview)
+    }
+    
+    // MARK: - Set Constraints
+    
+    private func setConstraints() {
+        
+    }
+}
