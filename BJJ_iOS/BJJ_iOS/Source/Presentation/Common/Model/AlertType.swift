0a004ef92d72386a9f6a2501bb558f4d3ef916a8
HyoTaek <kht1790@gmail.com>
2025-06-14 19:40:13 +0900

    [Feat] AlertType Enum으로 정의
    - success, failure로 대분류
    - Kind로 상세 분류
    - title에 각 종류에 따른 문구 저장

--- BJJ_iOS/BJJ_iOS/Source/Presentation/Common/Model/AlertType.swift
+++ BJJ_iOS/BJJ_iOS/Source/Presentation/Common/Model/AlertType.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+enum AlertType {
+    case success(Kind)
+    case failure(Kind)
+    
+    enum Kind {
+        case nicknameEdit
+    }
+}
+
+// TODO: 문구 수정
+
+extension AlertType {
+    var title: String {
+        switch self {
+        case .success(.nicknameEdit):
+            return "[테스트] 닉네임 수정 성공"
+        case .failure(.nicknameEdit):
+            return "[테스트] 닉네임 수정 실패"
+        }
+    }
+}
