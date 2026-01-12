1d6e068f5b0730ac7dfde40d12e4ac811bae3bac
HyoTaek <kht1790@gmail.com>
2026-01-13 00:13:52 +0900

    [UI] 아이콘 가진 버튼 재사용 컴포넌트 구현

--- BJJ_iOS/BJJ_iOS/Source/Presentation/Common/UIButton/IconConfirmButton.swift
+++ BJJ_iOS/BJJ_iOS/Source/Presentation/Common/UIButton/IconConfirmButton.swift
@@ -6,37 +6,62 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
-final class IconConfirmButton: UIButton {
+final class IconConfirmButton: BaseButton {
+
+    // MARK: - UI Components
+
+    private let iconImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
+    }
+    
+    private let title = UILabel()
     
     // MARK: - Init
     
     init(
         icon: ImageAsset,
-        title: String,
-        titleColor: UIColor = .black,
+        text: String,
+        titleColor: Colors = .black,
         backgroundColor: UIColor,
-        font: UIFont = .customFont(.pretendard_medium, 15)
+        font: Fonts = .pretendard_medium,
+        size: CGFloat = 15
     ) {
         super.init(frame: .zero)
         
-        setTitle(title, for: .normal)
-        setTitleColor(titleColor, for: .normal)
-        setImage(UIImage(named: icon.name), for: .normal)
-        titleLabel?.font = font
         self.backgroundColor = backgroundColor
         setCornerRadius(radius: 10)
         
-        // 아이콘과 텍스트 간격 설정
-        imageEdgeInsets = UIEdgeInsets(top: 0, left: -2, bottom: 0, right: 4)
-        titleEdgeInsets = UIEdgeInsets(top: 0, left: 4, bottom: 0, right: -2)
-        
-        // 아이콘 색상 설정
-        tintColor = titleColor
+        iconImageView.setImage(icon)
+        title.setLabelUI(text, font: font, size: size, color: titleColor)
     }
-
+    
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Set Hierarchy
+    
+    override func setHierarchy() {
+        [
+            iconImageView,
+            title
+        ].forEach(addSubview)
+    }
+    
+    // MARK: - Set Constraints
+    
+    override func setConstraints() {
+        iconImageView.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().offset(24)
+        }
+        
+        title.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
     }
 }
