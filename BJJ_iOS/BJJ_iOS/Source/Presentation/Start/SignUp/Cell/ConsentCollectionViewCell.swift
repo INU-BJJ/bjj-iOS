6b31d89f245cf1d633f58838329e28090da3b703
HyoTaek <kht1790@gmail.com>
2026-01-13 18:08:30 +0900

    [UI] 약관 동의 cell UI 구현

--- BJJ_iOS/BJJ_iOS/Source/Presentation/Start/SignUp/Cell/ConsentCollectionViewCell.swift
+++ BJJ_iOS/BJJ_iOS/Source/Presentation/Start/SignUp/Cell/ConsentCollectionViewCell.swift
@@ -5,4 +5,38 @@
 //  Created by HyoTaek on 1/13/26.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import Then
+
+final class ConsentCollectionViewCell: BaseCollectionViewCell<ConsentModel> {
+    
+    // MARK: - Components
+    
+    private let checkBoxIcon = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
+    }
+    
+    // MARK: - Set Hierarchy
+    
+    override func setHierarchy() {
+        [
+            checkBoxIcon
+        ].forEach(contentView.addSubview)
+    }
+    
+    // MARK: - Set Constraints
+    
+    override func setConstraints() {
+        checkBoxIcon.snp.makeConstraints {
+            $0.verticalEdges.equalToSuperview()
+            $0.size.equalTo(20)
+        }
+    }
+    
+    // MARK: - Configure Cell
+    
+    override func configureCell(with data: ConsentModel) {
+        checkBoxIcon.setImage(data.isAgreed ? .smallBorderCheckBox : .smallEmptyBorderCheckBox)
+    }
+}
