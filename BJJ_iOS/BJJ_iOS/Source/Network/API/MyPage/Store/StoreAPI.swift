2eedb9ad7b08bbba4f9911268afb338fefc26ea2
HyoTaek <kht1790@gmail.com>
2026-01-19 19:27:40 +0900

    [Networking] 전체 아이템 정보 가져오기 API 메서드의 파라미터 수정
    - string -> ItemType으로 타입안정성 확보

--- BJJ_iOS/BJJ_iOS/Source/Network/API/MyPage/Store/StoreAPI.swift
+++ BJJ_iOS/BJJ_iOS/Source/Network/API/MyPage/Store/StoreAPI.swift
@@ -8,13 +8,13 @@
 import Foundation
 
 final class StoreAPI {
-    static func fetchAllItems(itemType: String, completion: @escaping (Result<[StoreModel], Error>) -> Void) {
+    static func fetchAllItems(itemType: ItemType, completion: @escaping (Result<[StoreModel], Error>) -> Void) {
         networkRequest(
             urlStr: StoreAddress.fetchAllItems.url,
             method: .get,
             data: nil,
             model: [StoreModel].self,
-            query: ["itemType": itemType],
+            query: ["itemType": itemType.rawValue],
             completion: completion
         )
     }
