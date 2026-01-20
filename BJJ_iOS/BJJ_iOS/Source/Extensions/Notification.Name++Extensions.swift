bcba29d60fe6f9803a1fda23a18870e69c67a497
HyoTaek <kht1790@gmail.com>
2026-01-20 19:51:36 +0900

    [Feat] notificationCenter name 설정

---
+++ BJJ_iOS/BJJ_iOS/Source/Extensions/Notification.Name++Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  Notification.Name++Extensions.swift
+//  BJJ_iOS
+//
+//  Created by HyoTaek on 1/20/26.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let itemValidPeriodRefresh = Notification.Name("itemValidPeriodRefresh")
+}
