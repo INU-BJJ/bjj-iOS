3d38e0c63f1f10543b02e2bf6fa0404de8f2d35e
HyoTaek <kht1790@gmail.com>
2026-01-21 17:04:38 +0900

    [UI] 설정 셀 UI 구현

--- BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/MyPage/Setting/Cell/SettingTableViewCell.swift
+++ BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/MyPage/Setting/Cell/SettingTableViewCell.swift
@@ -5,4 +5,29 @@
 //  Created by HyoTaek on 1/21/26.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import Then
+
+final class SettingTableViewCell: BaseTableViewCell<String> {
+    
+    // MARK: Components
+    
+    private let titleLabel = UILabel().then {
+        $0.setLabel("", font: .pretendard_semibold, size: 15, color: .black)
+    }
+    
+    // MARK: - Set Hierarchy
+    
+    override func setHierarchy() {
+        contentView.addSubview(titleLabel)
+    }
+    
+    // MARK: - Set Constraints
+    
+    override func setConstraints() {
+        titleLabel.snp.makeConstraints {
+            $0.verticalEdges.leading.equalToSuperview()
+        }
+    }
+}
