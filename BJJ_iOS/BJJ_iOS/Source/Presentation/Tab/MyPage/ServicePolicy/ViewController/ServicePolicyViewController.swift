90abff83296b4a3f7055892556fd150f86a861bd
HyoTaek <kht1790@gmail.com>
2026-01-23 19:10:35 +0900

    [UI] 서비스 이용약관 페이지에 webView 추가

--- BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/MyPage/ServicePolicy/ViewController/ServicePolicyViewController.swift
+++ BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/MyPage/ServicePolicy/ViewController/ServicePolicyViewController.swift
@@ -8,13 +8,35 @@
 import UIKit
 import SnapKit
 import Then
+import WebKit
 
 final class ServicePolicyViewController: BaseViewController {
+
+    // MARK: - Components
+    
+    private let webView = WKWebView()
     
     // MARK: - Set UI
     
     override func setUI() {
         view.backgroundColor = .white
         setBackNaviBar("서비스 이용약관")
+        
+//        webView.loadHTMLString(htmlString, baseURL: baseURL)
+    }
+    
+    // MARK: - Set Hierarchy
+    
+    override func setHierarchy() {
+        view.addSubview(webView)
+    }
+    
+    // MARK: - Set Constraints
+    
+    override func setConstraints() {
+        webView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.horizontalEdges.bottom.equalToSuperview()
+        }
     }
 }
