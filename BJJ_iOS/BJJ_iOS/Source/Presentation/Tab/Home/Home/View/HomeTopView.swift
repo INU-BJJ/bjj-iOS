95833542d7519202ca8914cf65cb9546e12413e1
HyoTaek <kht1790@gmail.com>
2026-01-25 22:50:59 +0900

    [Refactor] HomeTopView를 BaseView 상속받도록 리팩토링

--- BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/Home/Home/View/HomeTopView.swift
+++ BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/Home/Home/View/HomeTopView.swift
@@ -9,52 +9,32 @@ import UIKit
 import SnapKit
 import Then
 
-final class HomeTopView: UIView {
+final class HomeTopView: BaseView {
     
-    // MARK: Properties
-    
-    private let jumbotronText = """
-                                  오늘의 인기 메뉴를
-                                  알아볼까요?
-                                """
-    // MARK: UI Components
+    // MARK: - Components
     
     private lazy var jumbotronLabel = UILabel().then {
-        $0.setLabelUI(jumbotronText, font: .pretendard_semibold, size: 24, color: .black)
+        $0.setLabelUI("오늘의 인기 메뉴", font: .pretendard_semibold, size: 24, color: .black)
         $0.numberOfLines = 0
     }
     
-    // MARK: LifeCycle
+    // MARK: - Set UI
         
-    init() {
-        super.init(frame: .zero)
-        
-        setUI()
-        setAddView()
-        setConstraints()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    // MARK: Set UI
-        
-    private func setUI() {
+    override func setUI() {
         backgroundColor = .customColor(.mainColor)
     }
     
-    // MARK: Set AddViews
+    // MARK: - Set Hierarchy
     
-    private func setAddView() {
+    override func setHierarchy() {
         [
          jumbotronLabel
         ].forEach(addSubview)
     }
     
-    // MARK: Set Constraints
+    // MARK: - Set Constraints
     
-    private func setConstraints() {
+    override func setConstraints() {
         jumbotronLabel.snp.makeConstraints {
             $0.top.equalToSuperview().offset(52)
             $0.leading.equalToSuperview().offset(31)
