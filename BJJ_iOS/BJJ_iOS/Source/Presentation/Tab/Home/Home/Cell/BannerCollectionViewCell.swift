1b593a882ae596ab01dfb7a6530b712831470ed8
HyoTaek <kht1790@gmail.com>
2026-01-25 23:44:11 +0900

    [Fix] svg 이미지 로딩 문제로 웹뷰로 배너 이미지 띄우도록 수정

--- BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/Home/Home/Cell/BannerCollectionViewCell.swift
+++ BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/Home/Home/Cell/BannerCollectionViewCell.swift
@@ -8,26 +8,26 @@
 import UIKit
 import SnapKit
 import Then
-import SDWebImage
+import WebKit
 
 final class BannerCollectionViewCell: BaseCollectionViewCell<Banner> {
     
     // MARK: - Components
     
-    private let bannerImageView = UIImageView().then {
-        $0.contentMode = .scaleAspectFit
+    private lazy var bannerWebView = WKWebView().then {
+        $0.scrollView.isScrollEnabled = false
     }
     
     // MARK: - Set Hierarchy
     
     override func setHierarchy() {
-        contentView.addSubview(bannerImageView)
+        contentView.addSubview(bannerWebView)
     }
     
     // MARK: - Set Constraints
     
     override func setConstraints() {
-        bannerImageView.snp.makeConstraints {
+        bannerWebView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
     }
@@ -35,10 +35,44 @@ final class BannerCollectionViewCell: BaseCollectionViewCell<Banner> {
     // MARK: - Configure Cell
     
     override func configureCell(with data: Banner) {
-        bannerImageView.sd_setImage(
-            with: URL(string: baseURL.bannerImageURL + data.image),
-            placeholderImage: nil,
-            options: [.retryFailed, .continueInBackground]
-        )
+        let svgURL = baseURL.bannerImageURL + data.image
+        let html = createBannerHTML(with: svgURL)
+
+        bannerWebView.loadHTMLString(html, baseURL: nil)
+    }
+
+    // MARK: - Create Banner HTML
+
+    private func createBannerHTML(with url: String) -> String {
+        return """
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+            <style>
+                * {
+                    margin: 0;
+                    padding: 0;
+                }
+                body {
+                    width: 100vw;
+                    height: 182px;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    overflow: hidden;
+                }
+                img {
+                    width: 100%;
+                    height: 100%;
+                    object-fit: contain;
+                }
+            </style>
+        </head>
+        <body>
+            <img src="\(url)" alt="Banner">
+        </body>
+        </html>
+        """
     }
 }
