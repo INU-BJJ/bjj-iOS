b7a8f4bc8da154110c59f87544ea7c4989806bde
HyoTaek <kht1790@gmail.com>
2026-01-26 22:57:22 +0900

    [Feat] 신고viewModel 생성 및 reviewID 초기화

---
+++ BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/Review/ReportReview/ViewModel/ReportReviewViewModel.swift
@@ -0,0 +1,44 @@
+//
+//  ReportReviewViewModel.swift
+//  BJJ_iOS
+//
+//  Created by HyoTaek on 1/26/26.
+//
+
+import RxSwift
+import RxCocoa
+
+final class ReportReviewViewModel: BaseViewModel {
+    
+    // MARK: - DisposeBag
+    
+    private let disposeBag = DisposeBag()
+    
+    // MARK: - Properties
+    
+    private let reviewID: Int
+    
+    // MARK: - Init
+    
+    init(reviewID: Int) {
+        self.reviewID = reviewID
+    }
+    
+    // MARK: - Input
+    
+    struct Input {
+        
+    }
+    
+    // MARK: - Output
+    
+    struct Output {
+        
+    }
+    
+    // MARK: - Transform
+    
+    func transform(input: Input) -> Output {
+        return Output()
+    }
+}
