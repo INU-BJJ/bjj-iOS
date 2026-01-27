c91560c7205ce8b7a82fe5143b83457c1630fbf1
HyoTaek <kht1790@gmail.com>
2026-01-27 21:21:31 +0900

    [Refactor] ReviewPhotoCell을 baseCollectionViewCell 상속받도록 리팩토링

--- BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/Home/ReviewPhotoGallery/View/ReviewPhotoCell.swift
+++ BJJ_iOS/BJJ_iOS/Source/Presentation/Tab/Home/ReviewPhotoGallery/View/ReviewPhotoCell.swift
@@ -10,44 +10,31 @@ import SnapKit
 import Then
 import Kingfisher
 
-final class ReviewPhotoCell: UICollectionViewCell, ReuseIdentifying {
-    
-    // MARK: - Properties
+final class ReviewPhotoCell: BaseCollectionViewCell<String> {
     
     // MARK: - UI Components
     
-    private let reviewPhotoImageView = UIImageView()
-    
-    // MARK: - Life Cycle
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-        setAddView()
-        setConstraints()
+    private let reviewPhotoImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    // MARK: - Set Hierarchy
     
-    // MARK: - Set AddView
-    
-    private func setAddView() {
+    override func setHierarchy() {
         contentView.addSubview(reviewPhotoImageView)
     }
     
     // MARK: - Set Constraints
     
-    private func setConstraints() {
+    override func setConstraints() {
         reviewPhotoImageView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
     }
     
-    // MARK: - Set UI
+    // MARK: - Configure Cell
     
-    func setUI(reviewPhoto: String) {
-        reviewPhotoImageView.kf.setImage(with: URL(string: "\(baseURL.reviewImageURL)\(reviewPhoto)"))
+    override func configureCell(with data: String) {
+        reviewPhotoImageView.kf.setImage(with: URL(string: "\(baseURL.reviewImageURL)\(data)"))
     }
 }
