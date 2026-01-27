6ff6cbb35e4f8946fb2b24c787d90f766dce7075
HyoTaek <kht1790@gmail.com>
2026-01-28 00:17:37 +0900

    [Feat] UserDefaultsManager 구현

---
+++ BJJ_iOS/BJJ_iOS/Source/Manager/UserDefaultsManager.swift
@@ -0,0 +1,40 @@
+//
+//  UserDefaultsManager.swift
+//  BJJ_iOS
+//
+//  Created by HyoTaek on 1/27/26.
+//
+
+import Foundation
+
+final class UserDefaultsManager {
+    
+    // MARK: - Key
+
+    enum UserDefaultsKey: String {
+        case hasRequestedNotificationPermission = "HasRequestedNotificationPermission"
+    }
+    
+    // MARK: - Init
+    
+    static let shared = UserDefaultsManager()
+    private init() { }
+    
+    // MARK: - Save
+    
+    func save<T>(value: T, key: UserDefaultsKey) {
+        UserDefaults.standard.set(value, forKey: key.rawValue)
+    }
+    
+    // MARK: - Read
+    
+    func readBool(_ key: UserDefaultsKey) -> Bool {
+        return UserDefaults.standard.bool(forKey: key.rawValue)
+    }
+    
+    // MARK: - Delete
+    
+    func delete(_ key: UserDefaultsKey) {
+        UserDefaults.standard.removeObject(forKey: key.rawValue)
+    }
+}
